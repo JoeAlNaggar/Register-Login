fcd362dfe334b17dc781109190be1dd32127242d
Yousef AlNaggar <115173858+JoeAlNaggar@users.noreply.github.com>
2023-10-22 17:38:57 +0300

    Add files via upload

---
+++ Register-Login/js/Login.js
@@ -0,0 +1,20 @@
+const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+function checkData(event) {
+        event.preventDefault();
+      
+        const loginEmail = document.getElementById("email").value;
+        const loginPassword = document.getElementById("password").value;
+      
+        const user = existingUsers.find(user => user.email === loginEmail && user.password === loginPassword);
+      
+        if (user) {
+          alert("Login Successful.");
+          window.location.href = "home.html";
+
+        } else {
+          alert("Login failed. Invalid email or password.");
+        }
+}
+
+document.getElementById("loginForm").addEventListener("submit", checkData); 
